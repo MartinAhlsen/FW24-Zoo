5802fbbc17c671927267a6058214a0fd5906dc1c
MartinAhlsen <martin.ahlsen@gmail.com>
2024-07-11 22:16:33 +0200

    some changes?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Link, NavLink } from "react-router-dom";
 import "./App.css";
 import Birds from "./Pages/Birds";
@@ -7,16 +7,26 @@ import Reptiles from "./Pages/Reptiles";
 import Home from "./Pages/Home";
 import NoPage from "./Pages/NoPage";
 import Nav from "./Components/Nav";
+import Data from "./assets/Data/data";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Ladda in all data
+    //skicka in datan som props
+  }, []);
+
   return (
     <>
       <h1>Hello world</h1>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/birds" element={<Birds />}></Route>
+        <Route
+          path="/birds"
+          element={<Birds data={loadedData["birds"]} />}
+        ></Route>
         <Route path="/mammals" element={<Mammals />}></Route>
         <Route path="/reptiles" element={<Reptiles />}></Route>
         <Route path="*" element={<NoPage />}></Route>
--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,23 +1,49 @@
 import { NavLink } from "react-router-dom";
+import styles from "./nav.module.css";
 
 const Nav = () => {
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/birds">Birds</NavLink>
+          <NavLink
+            to="/birds"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Birds
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/mammals">Mammals</NavLink>
+          <NavLink
+            to="/mammals"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Mammals
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/reptiles">Reptiles</NavLink>
+          <NavLink
+            to="/reptiles"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Reptiles
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/asdfasdf">404</NavLink>
+          <NavLink
+            to="/asdfasdf"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            404
+          </NavLink>
         </li>
       </ul>
     </nav>
